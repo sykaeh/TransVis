6e145c9ab06b657f3d28622bbd852bea82146f21
Sybil Ehrensberger <sybil.ehrensberger@gmail.com>
2014-11-05 16:07:34 +0100

    Add Javadoc and format

--- src/transvis/Transcript.java
+++ src/transvis/Transcript.java
@@ -11,44 +11,49 @@ import java.util.LinkedList;
 import java.util.List;
 
 /**
+ * Base class for each transcript and thus each individual process.
+ *
  * @author Sybil Ehrensberger
  */
 public class Transcript {
 
-    /* The name of the file to be analyzed. */
-    public String name; // name as specified in the name attribute of document
-
-
+    /** The name of the file to be analyzed (also specified in the name attribute) */
+    public String name;
+    /** Participant ID */
     public String participant;
+    /** Group the participant belongs to */
     public String group;
+    /** Competence level (i.e. Beg, Pro) */
     public String competence;
+    /** Language version */
     public String version;
+    /** Name of the source text */
     public String sourcetextname;
-
+    /** Recording information added in the XML file */
     public Recording recording = null;
 
-
-    /* Adjustment time [in sec] */
+    /** Adjustment time [in sec] */
     public int startAdjustment;
-
-    /* Total time of process (specified by start & end in recording tag) */
+    /** Total time of process (specified by start & end in recording tag) */
     public int totalTime;
-
-    /* beginning of the drafting phase (first write incident) */
+    /** Beginning of the drafting phase (first write incident) */
     public Integer startDrafting = null;
-
-    /* beginning of the revision phase (specified in recording tag) */
+    /** Beginning of the revision phase (specified in recording tag) */
     public int startRevision;
 
-
+    /** Whether this transcript is from a workplace environment */
     public boolean workPlace;
 
-
+    /** Name of the selection (e.g. Complete Process, Orientation Phase, ...) */
     public String selection;
-    public int startSelection; // beginning of the selected timespan
-    public int endSelection; // end of the selected timespan
+    /** Beginning of the selection [in sec] */
+    public int startSelection;
+    /** End of the selection [in sec] */
+    public int endSelection;
+    /** Total duration of the selection [in sec] */
     public int durationSelection;
 
+    /** List of all found incidents */
     List<BaseIncident> incidents;
 
     /**
@@ -70,7 +75,11 @@ public class Transcript {
 
     }
 
-
+    /**
+     * Parse the name attribute of the document in to individual parts and throw a fatal error if is malformed.
+     *
+     * @param n the name attribute from the XML file
+     */
     public void setName(String n) {
 
         name = n;
@@ -85,6 +94,11 @@ public class Transcript {
             fatal("Invalid name attribute: " + name);
     }
 
+    /**
+     * Add the recording information to the transcript and validate it. If it is not valid, throw a fatal error.
+     *
+     * @param r the recording information to be added
+     */
     public void addRecording(Recording r) {
 
         if (recording != null) {
@@ -113,22 +127,13 @@ public class Transcript {
     }
 
     /**
-     * Converts the given timestring of the format HH:MM:SS to seconds
-     * (an int).
+     * Set the selection according to the given type and given start and end times. Filter the incidents accordingly
+     * so only incidents within that time span will be used.
      *
-     * @param timestring the time (format HH:MM:SS)
-     * @return the elapsed time in seconds
+     * @param type number indicating which type was chosen in the GUI (0: partial, 1: complete, 2: orientation, 3: drafting, 4: revision)
+     * @param start the start time in seconds for partial processes
+     * @param end the end time in seconds for partial processes
      */
-    public static int convertToSeconds(String timestring) {
-
-        String[] times = timestring.trim().split(":");
-        int time = Integer.parseInt(times[0]) * 3600
-                + Integer.parseInt(times[1]) * 60 + Integer.parseInt(times[2]);
-
-        return time;
-    }
-
-
     public void setSelection(int type, int start, int end) {
 
         // TODO: This does not seem to do anything when showing the graph!
@@ -189,11 +194,33 @@ public class Transcript {
      */
     public int adjustTime(String timestring) {
         int time = convertToSeconds(timestring);
-        return time - startAdjustment;
+        return adjustTime(time);
     }
 
+    /**
+     * Adjust the given time t to ignore the warm-up phase.
+     *
+     * @param t the time in seconds
+     * @return the adjusted time in seconds
+     */
     public int adjustTime(int t) {
         return t - startAdjustment;
+    }
+
+    /**
+     * Converts the given timestring of the format HH:MM:SS to seconds
+     * (an int).
+     *
+     * @param timestring the time (format HH:MM:SS)
+     * @return the elapsed time in seconds
+     */
+    public static int convertToSeconds(String timestring) {
+
+        String[] times = timestring.trim().split(":");
+        int time = Integer.parseInt(times[0]) * 3600
+                + Integer.parseInt(times[1]) * 60 + Integer.parseInt(times[2]);
+
+        return time;
     }
 
 }
